00e055403ac35bb891ba36180dcf4ebd7e8402f9
oczkoisse <oczkoisse@pm.me>
2021-08-12 00:52:32 -0700

    Add documentation to MainWindow module

--- src/labeling_tool/MainWindow.py
+++ src/labeling_tool/MainWindow.py
@@ -1,10 +1,7 @@
-from typing import Tuple
-
+"""Main window of application."""
 from PySide6 import QtWidgets as qtw
 from PySide6 import QtCore as qtc
-from PySide6 import QtGui as qtg
 import av
-import av.video
 import numpy as np
 
 from labeling_tool.Ui_MainWindow import Ui_MainWindow
@@ -12,12 +9,13 @@ from labeling_tool.VideoTimer import VideoTimer
 
 
 class MainWindow(qtw.QMainWindow):
-    """Main window of our video player"""
+    """Main window of application."""
 
     video_files_filter = "Videos (*.mkv *.avi *.mp4 *.mov)"
     all_files_filter = "All files (*)"
 
     def __init__(self):
+        """Create the window."""
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
@@ -28,6 +26,7 @@ class MainWindow(qtw.QMainWindow):
 
     @qtc.Slot()
     def on_file_open(self):
+        """Handle file open dialog."""
         file_path, _ = qtw.QFileDialog.getOpenFileName(
             self,
             "Choose video",
@@ -48,9 +47,20 @@ class MainWindow(qtw.QMainWindow):
 
 
 class Decoder(qtc.QObject):
+    """Decoder to read a video and emit decoded frames via signals.
+
+    Attributes:
+        decoded (PySide6.QtCore.Signal): Finished decoding
+    """
+
     decoded = qtc.Signal(float, tuple)
 
-    def __init__(self, file_path):
+    def __init__(self, file_path: str):
+        """Open the video at given path for decoding.
+
+        Args:
+            file_path: Path to video file
+        """
         super().__init__()
         self._path = file_path
         self._container = av.open(self._path, mode="r")
@@ -60,6 +70,11 @@ class Decoder(qtc.QObject):
         self._decoder = self._container.decode(video=0)
 
     def on_decode(self):
+        """Handle decode signal.
+
+        Raises:
+            ValueError: If the opened video file format is not supported
+        """
         frame = next(self._decoder, None)
         if frame is None:
             return
@@ -68,17 +83,18 @@ class Decoder(qtc.QObject):
             # Only supported pixel format are yuv420p and yuvj420p
             # yuvj420p is simply yuv420p but with full colors (0-255)
             raise ValueError(
-                f"Unsupported pixel format '{frame.format.name} 'in video. Only yuv420p/yuvj420p videos are supported."
+                f"Unsupported pixel format '{frame.format.name} 'in video. "
+                "Only yuv420p/yuvj420p videos are supported."
             )
 
         y, cb, cr = map(self._remove_padding, frame.planes)
         self.decoded.emit(frame.time, (y, cb, cr))
 
-    def _remove_padding(self, plane):
+    def _remove_padding(self, plane: av.video.plane.VideoPlane):
         """Remove padding from a video frame's plane.
 
         Args:
-            plane (av.video.plane.VideoPlane): the plane to remove padding from
+            plane: the plane to remove padding from
 
         Returns:
             numpy.array: 2D array representing the plane data sans the padding
@@ -94,6 +110,7 @@ class Decoder(qtc.QObject):
 
 
 def main():
+    """Start the application and enter Qt event loop."""
     import sys
 
     app = qtw.QApplication(sys.argv)
